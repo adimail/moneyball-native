879fee9f2dbb21336d06c4919e2575e72f2d6d02
Aditya <adimail2404@gmail.com>
2024-02-10 00:51:59 +0530

    Initial Commit: Config setup

---
+++ src/config.js
@@ -0,0 +1,19 @@
+const defaultAvatar =
+  'https://firebasestorage.googleapis.com/v0/b/expo-boilerplate-v2.appspot.com/o/icon.png?alt=media&token=505f30e6-6702-410a-84e7-f66b3ff60daf'
+
+const firebaseKey = {
+  apiKey: 'AIzaSyDMFtbbgUhrxIrNj-BEG2fPGlScjn6vFy8',
+  authDomain: 'moneyball-aea9d.firebaseapp.com',
+  projectId: 'moneyball-aea9d',
+  storageBucket: 'moneyball-aea9d.appspot.com',
+  messagingSenderId: '1043203319105',
+  appId: '1:1043203319105:web:5c154d64bfb79bfc286dc0',
+  measurementId: 'G-FL96KJQYV8',
+}
+
+const eulaLink =
+  'https://github.com/kiyohken2000/ReactNative-Expo-Firebase-Boilerplate-v2'
+
+const expoProjectId = '1650611b-a5b8-4420-9656-60c06429edd4'
+
+export { defaultAvatar, firebaseKey, eulaLink, expoProjectId }
