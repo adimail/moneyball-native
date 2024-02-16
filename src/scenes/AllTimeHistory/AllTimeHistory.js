db01b99a29d65316d6e21f3bac0fd1babb2a0af4
Aditya <adimail2404@gmail.com>
2024-02-16 17:22:41 +0530

    Read months passes since joined

--- src/scenes/AllTimeHistory/AllTimeHistory.js
+++ src/scenes/AllTimeHistory/AllTimeHistory.js
@@ -1,34 +1,54 @@
-import React, { useEffect, useContext, useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import {
   Text,
   View,
   StyleSheet,
   ScrollView,
+  Alert,
   RefreshControl,
 } from 'react-native'
 import ScreenTemplate from '../../components/ScreenTemplate'
-import { colors } from 'theme'
+import { firestore } from '../../firebase/config'
+import { doc } from 'firebase/firestore'
 import { ColorSchemeContext } from '../../context/ColorSchemeContext'
 import { UserDataContext } from '../../context/UserDataContext'
-import { collection, getDocs } from 'firebase/firestore'
-import { firestore } from '../../firebase/config'
-import { showToast } from '../../utils/ShowToast'
+import { colors, fontSize } from '../../theme'
 
 export default function AllTimeHistory() {
-  const { userData } = useContext(UserDataContext)
+  const { userData, setUserData } = useContext(UserDataContext)
   const { scheme } = useContext(ColorSchemeContext)
   const isDark = scheme === 'dark'
   const colorScheme = {
     text: isDark ? colors.white : colors.primaryText,
   }
-  const [isRefreshing, setIsRefreshing] = useState(false)
+  const [refreshing, setRefreshing] = useState(false)
 
-  const joinedDate = new Date(userData.joined)
+  const [monthsSinceJoined, setMonthsSinceJoined] = useState([])
+
+  const joinedDate = userData.joined.toDate()
+  const currentDate = new Date()
+
+  useEffect(() => {
+    let currentMonth = new Date(joinedDate)
+    const months = []
+
+    while (currentMonth <= currentDate) {
+      const formattedMonth = currentMonth.toLocaleDateString('en-GB', {
+        month: 'short',
+        year: 'numeric',
+      })
+
+      months.push(formattedMonth)
+      currentMonth.setMonth(currentMonth.getMonth() + 1)
+    }
+
+    setMonthsSinceJoined(months)
+  }, [userData.joined])
 
   const onRefresh = () => {
-    setIsRefreshing(true)
+    setRefreshing(true)
     setTimeout(() => {
-      setIsRefreshing(false)
+      setRefreshing(false)
     }, 2000)
   }
 
@@ -38,21 +58,57 @@ export default function AllTimeHistory() {
         showsVerticalScrollIndicator={false}
         style={{ paddingTop: 20 }}
         refreshControl={
-          <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
-      ></ScrollView>
+      >
+        <View>
+          <Text style={[styles.title, { color: colorScheme.text }]}>
+            Months since joined:
+          </Text>
+          {monthsSinceJoined.map((month, index) => (
+            <Text
+              key={index}
+              style={[styles.title, { color: colorScheme.text }]}
+            >
+              {month}
+            </Text>
+          ))}
+        </View>
+      </ScrollView>
     </ScreenTemplate>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
+    flex: 1,
+    width: '100%',
+  },
+  title: {
+    fontSize: fontSize.xxxLarge,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  field: {
+    fontSize: fontSize.middle,
+    textAlign: 'center',
+  },
+  avatar: {
+    margin: 30,
+    alignSelf: 'center',
+    borderWidth: 7,
+    borderColor: 'gray',
+    borderRadius: 500,
+  },
+  footerView: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 20,
+    marginTop: 20,
   },
-  text: {
-    fontSize: 18,
-    marginBottom: 10,
+  footerLink: {
+    color: colors.blueLight,
+    fontWeight: 'bold',
+    fontSize: fontSize.large,
   },
 })
